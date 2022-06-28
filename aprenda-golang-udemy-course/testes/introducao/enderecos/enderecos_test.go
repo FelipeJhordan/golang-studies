5726774ebf029095f46878044a78f1eddb11965e
felipejhordan <felipejordan.alves@gmail.com>
2022-06-28 16:06:29 -0300

    feat: increase the last test

--- aprenda-golang-udemy-course/testes/introducao/enderecos/enderecos_test.go
+++ aprenda-golang-udemy-course/testes/introducao/enderecos/enderecos_test.go
@@ -2,16 +2,49 @@ package enderecos
 
 import "testing"
 
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
 // TestXxxxxxx
 func TestTipoEndereco(t *testing.T) {
-	enderecoParaTeste := "Avenida Paulista"
+	cenarioDeTeste := []cenarioDeTeste{
+		{
+			enderecoInserido: "Rua ABC",
+			retornoEsperado:  "Rua",
+		},
+		{
+			enderecoInserido: "Avenida Brasil",
+			retornoEsperado:  "Avenida",
+		},
+		{
+			enderecoInserido: "Praça Das Rosas",
+			retornoEsperado:  "Tipo não especificado",
+		},
+		{
+			enderecoInserido: "Rodovia 150",
+			retornoEsperado:  "Rodovia",
+		},
+		{
+			enderecoInserido: "Estrada da Vila",
+			retornoEsperado:  "Estrada",
+		},
+		{
+			enderecoInserido: "RUA DOS BOBOS",
+			retornoEsperado:  "Rua",
+		},
+		{
+			enderecoInserido: "",
+			retornoEsperado:  "Tipo não especificado",
+		},
+	}
 
-	TipoDeEnderecoEsperado := "Avenida"
-
-	tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
-
-	if tipoDeEnderecoRecebido != TipoDeEnderecoEsperado {
-		t.Errorf("O tipo recebido é diferente do esperado ! Esperava %s e recebeu %s",
-			TipoDeEnderecoEsperado, tipoDeEnderecoRecebido)
+	for _, cenario := range cenarioDeTeste {
+		TipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if TipoDeEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s",
+				TipoDeEnderecoRecebido, cenario.retornoEsperado)
+		}
 	}
 }
