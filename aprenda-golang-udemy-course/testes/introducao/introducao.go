3f9a21263d26ca7596582f34de6e83de317ace50
felipejhordan <felipejordan.alves@gmail.com>
2022-06-28 15:46:23 -0300

    feat: create a base func to create tests about it

---
+++ aprenda-golang-udemy-course/testes/introducao/introducao.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"introducao-testes/enderecos"
+)
+
+func main() {
+	tipoEndereco := enderecos.TipoDeEndereco("Avendasdsaida Paulista")
+	fmt.Println(tipoEndereco)
+}
