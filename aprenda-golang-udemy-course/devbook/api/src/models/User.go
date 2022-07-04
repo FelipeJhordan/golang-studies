aa5009c148cd1659354de8dfa6ba4cce7f0407ba
felipejhordan <felipejordan.alves@gmail.com>
2022-07-04 19:26:30 -0300

    refactor: change modelo folder to models

--- aprenda-golang-udemy-course/devbook/api/src/models/User.go
+++ aprenda-golang-udemy-course/devbook/api/src/models/User.go
@@ -1,8 +1,8 @@
-package modelo
+package models
 
 import "time"
 
-type Usuario struct {
+type User struct {
 	ID        uint64    `json:"id,omitempty"`
 	Name      string    `json:"name,omitempty"`
 	Nick      string    `json:"nick, omitempty"`
