8a70a5e6dfe64156b6184b1f8f46ab3b20103803
felipejhordan <felipejordan.alves@gmail.com>
2022-07-04 19:26:54 -0300

    feat: create user repository

--- aprenda-golang-udemy-course/devbook/api/src/controllers/users-controllers.go
+++ aprenda-golang-udemy-course/devbook/api/src/controllers/users-controllers.go
@@ -1,19 +1,43 @@
 package controllers
 
-import "net/http"
+import (
+	db "api/src/bd"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-func CreateUser(w http.ResponseWriter, R *http.Request) {
-	w.Write([]byte("Criando usuários"))
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	bodyRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var user models.User
+	if erro = json.Unmarshal(bodyRequest, &user); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := db.Connect()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repository := repositories.CreateNewUserRepository(db)
+	repository.Create(user)
 }
-func ListUsers(w http.ResponseWriter, R *http.Request) {
+func ListUsers(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Listar usuários"))
 }
-func FindUser(w http.ResponseWriter, R *http.Request) {
+func FindUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Buscar usuário"))
 }
-func UpdateUser(w http.ResponseWriter, R *http.Request) {
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Atualizar Usuário"))
 }
-func DeleteUser(w http.ResponseWriter, R *http.Request) {
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Deletar Usuário"))
 }
