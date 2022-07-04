ebfbe4d0e217a2ecd7d78b59b5e0945126c8eb1f
felipejhordan <felipejordan.alves@gmail.com>
2022-07-04 16:47:10 -0300

    feat: create a handle func to insert all routes in router container and use this function when the router is created

--- aprenda-golang-udemy-course/devbook/api/src/router/routes/routes.go
+++ aprenda-golang-udemy-course/devbook/api/src/router/routes/routes.go
@@ -1,10 +1,24 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Route struct {
 	URI                string
 	Method             string
 	Function           func(http.ResponseWriter, *http.Request)
 	NeedAuthentication bool
+}
+
+func ConfigureRoutes(router *mux.Router) *mux.Router {
+	routes := routesUsers
+
+	for _, route := range routes {
+		router.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return router
 }
