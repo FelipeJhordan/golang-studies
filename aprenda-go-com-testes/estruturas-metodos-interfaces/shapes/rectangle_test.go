b6cbb84e5b3558307117a80f3f86b10f76873405
FelipeJhordan <felipejordan.alves@gmail.com>
2022-11-02 19:01:19 -0300

    test(learn-with-tests/structs): make the test to get the area by width and height

--- aprenda-go-com-testes/estruturas-metodos-interfaces/shapes/rectangle_test.go
+++ aprenda-go-com-testes/estruturas-metodos-interfaces/shapes/rectangle_test.go
@@ -10,3 +10,13 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("resultado %.2f esperado %.2f", result, expect)
 	}
 }
+
+func TestArea(t *testing.T) {
+	result := Area(12.0, 6.0)
+
+	expect := 72.0
+
+	if result != expect {
+		t.Errorf("resultado %.2f, esperado %.2f", result, expect)
+	}
+}
