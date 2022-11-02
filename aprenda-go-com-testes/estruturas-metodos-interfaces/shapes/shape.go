3c8da34026b0fb4349db5e083b96cbacff6dd368
FelipeJhordan <felipejordan.alves@gmail.com>
2022-11-02 19:46:56 -0300

    feat(learn-with-tests/structs): make the minimium of code to test pass

--- aprenda-go-com-testes/estruturas-metodos-interfaces/shapes/shape.go
+++ aprenda-go-com-testes/estruturas-metodos-interfaces/shapes/shape.go
@@ -23,6 +23,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
+type Triangle struct {
+	base   float64
+	height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.width + rectangle.height)
 }
