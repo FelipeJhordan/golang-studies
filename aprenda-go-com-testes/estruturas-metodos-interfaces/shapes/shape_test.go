ffd7115cb714c89d6f7be3198b2f761493ba61df
FelipeJhordan <felipejordan.alves@gmail.com>
2022-11-02 19:57:57 -0300

    refactor(learn-with-tests/structs): improve the tests-switch

--- aprenda-go-com-testes/estruturas-metodos-interfaces/shapes/shape_test.go
+++ aprenda-go-com-testes/estruturas-metodos-interfaces/shapes/shape_test.go
@@ -14,30 +14,39 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	testsArea := []struct {
+		name   string
 		shape  Shape
 		expect float64
 	}{
 		{
-			Rectangle{12, 6}, 72.00,
+			name:   "Retangulo",
+			shape:  Rectangle{width: 12, height: 6},
+			expect: 72.00,
 		},
 		{
-			Circle{
-				10,
-			}, 314.1592653589793,
+			name: "Circle",
+			shape: Circle{
+				radius: 10,
+			},
+			expect: 314.1592653589793,
 		},
 		{
-			Triangle{
-				12,
-				6,
-			}, 36.00,
+			name: "Triangle",
+			shape: Triangle{
+				base:   12,
+				height: 6,
+			},
+			expect: 36.00,
 		},
 	}
 
 	for _, tt := range testsArea {
-		result := tt.shape.Area()
+		t.Run(tt.name, func(t *testing.T) {
+			result := tt.shape.Area()
+			if result != tt.expect {
+				t.Errorf("%#v resultado %.2f, esperado %.2f", tt.shape, result, tt.expect)
+			}
+		})
 
-		if result != tt.expect {
-			t.Errorf("resultado %.2f, esperado %.2f", result, tt.expect)
-		}
 	}
 }
