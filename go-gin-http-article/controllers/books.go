26c366901038480a53fe152f42bc681494bc0144
Felipe Jhordan <44248690+FelipeJhordan@users.noreply.github.com>
2023-01-08 23:34:49 -0300

    feat(go-gin): create the controller with all crud operations

---
+++ go-gin-http-article/controllers/books.go
@@ -0,0 +1,72 @@
+package controllers
+
+import (
+	"net/http"
+
+	. "example.com/go-gin/controllers/inputs"
+	"example.com/go-gin/models"
+	"github.com/gin-gonic/gin"
+)
+
+func FindBooks(c *gin.Context) {
+	var books []models.Book
+
+	models.DB.Find(&books)
+
+	c.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+func CreateBook(c *gin.Context) {
+	var input CreateBookInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	book := models.Book{Title: input.Title, Author: input.Author}
+	models.DB.Create(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func FindBook(c *gin.Context) { // Get model if exist
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func UpdateBook(c *gin.Context) {
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.DB.Model(&book).Updates(input)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func DeleteBook(c *gin.Context) {
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	models.DB.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
+}
