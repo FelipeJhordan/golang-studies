5a912952063b74ada7d61f8a03a5a6b0e94090d8
FelipeJhordan <felipejordan.alves@gmail.com>
2023-07-09 21:56:42 -0300

    feat(graphql-fullcycle):  implement relactions

--- golang-graphql-full-cycle/graph/schema.resolvers.go
+++ golang-graphql-full-cycle/graph/schema.resolvers.go
@@ -10,6 +10,39 @@ import (
 	"github.com/devfullcycle/13-GraphQL/graph/model"
 )
 
+// Courses is the resolver for the courses field.
+func (r *categoryResolver) Courses(ctx context.Context, obj *model.Category) ([]*model.Course, error) {
+	courses, err := r.CourseDB.FindByCategoryId(obj.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	var coursesModel []*model.Course
+	for _, course := range courses {
+		coursesModel = append(coursesModel, &model.Course{
+			ID:          course.ID,
+			Name:        course.Name,
+			Description: &course.Description,
+		})
+	}
+
+	return coursesModel, nil
+}
+
+// Category is the resolver for the category field.
+func (r *courseResolver) Category(ctx context.Context, obj *model.Course) (*model.Category, error) {
+	category, err := r.CategoryDB.FindByCourseID(obj.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Category{
+		ID:          category.ID,
+		Name:        category.Name,
+		Description: category.Name,
+	}, nil
+}
+
 // CreateCategory is the resolver for the createCategory field.
 func (r *mutationResolver) CreateCategory(ctx context.Context, input *model.NewCategory) (*model.Category, error) {
 	category, err := r.CategoryDB.Create(input.Name, *input.Description)
@@ -20,7 +53,7 @@ func (r *mutationResolver) CreateCategory(ctx context.Context, input *model.NewC
 	return &model.Category{
 		ID:          category.ID,
 		Name:        category.Name,
-		Description: &category.Description,
+		Description: category.Description,
 	}, nil
 }
 
@@ -49,8 +82,8 @@ func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, erro
 	for _, category := range categories {
 		categoriesModel = append(categoriesModel, &model.Category{
 			ID:          category.ID,
-			Description: &category.Description,
-			Name:        category.Description,
+			Description: category.Description,
+			Name:        category.Name,
 		})
 	}
 
@@ -76,11 +109,19 @@ func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
 	return coursesModel, nil
 }
 
+// Category returns CategoryResolver implementation.
+func (r *Resolver) Category() CategoryResolver { return &categoryResolver{r} }
+
+// Course returns CourseResolver implementation.
+func (r *Resolver) Course() CourseResolver { return &courseResolver{r} }
+
 // Mutation returns MutationResolver implementation.
 func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 
 // Query returns QueryResolver implementation.
 func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
+type categoryResolver struct{ *Resolver }
+type courseResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
